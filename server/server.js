c4c94861a36dd07e1e4a43c56bdb38d2d228a20b
karlculi44 <kaimax187@gmail.com>
2025-10-16 14:23:38 +0800

    Connected Database
    Connected the database using mongodb and mongoose.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express";
 import dotenv from 'dotenv';
 import router from "./routes/users.js";
 import errorHandler from "./middlewares/error.js";
+import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
@@ -16,11 +17,21 @@ app.use(express.urlencoded({ extended: false }));
 // Uses the router middleware
 app.use(router);
 
+// Connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+
+    // Listens only for requests once database connection has been established.
+    app.listen(PORT, () => console.log(`PORT is running on PORT ${PORT}`));
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 // Error handling middleware
 app.use(errorHandler);
 
+// main endpoint
 app.get('/', (req, res) => {
   return res.send("Welcome to the MERN Stack App.");
 });
-
-app.listen(PORT, () => console.log(`PORT is running on PORT ${PORT}`));
