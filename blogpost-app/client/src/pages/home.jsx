9c28825d4275e5abe9943bbff6c241832ba9e53d
karlculi44 <kaimax187@gmail.com>
2025-10-20 09:30:03 +0800

    Changed styles
    Reformatted the user render style.

--- blogpost-app/client/src/pages/home.jsx
+++ blogpost-app/client/src/pages/home.jsx
@@ -18,14 +18,19 @@ const Home = () => {
 
   return (
     <>
-      {users && users.map(user => (
-        <div key={user.id || user.username} className="bg-white dark:bg-zinc-900 border border-gray-200 dark:border-zinc-700 rounded-xl p-4 shadow-sm hover:shadow-md transition-all duration-300">
-          <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
-            {user.fullName}
-          </h2>
-          <p className="text-gray-500 dark:text-gray-400">@{user.username}</p>
+      <div className="flex justify-center">
+        <div className="max-w-[80%]">
+          {users && users.map(user => (
+            <div key={user.id || user.username} className="bg-white dark:bg-zinc-900 border border-gray-200 dark:border-zinc-700 rounded-xl p-4 shadow-sm hover:shadow-md transition-all duration-300">
+              <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
+                {user.fullName}
+              </h2>
+              <p className="text-gray-500 dark:text-gray-400">@{user.username}</p>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
+
     </>
   );
 }
